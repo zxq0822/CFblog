074cc384b60716158e85e454b4c213e9b96f0bef
zxq0822 <141250783+zxq0822@users.noreply.github.com>
2025-01-02 00:06:47 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,8 +59,8 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © 2025/01/01 Powered by <a className='hover:underline' href="https://lizhi.us.kg" target="_blank">Qing</a>
-                        \n知不可乎骤得,托遗响于悲风。
+                        © 2025/01/01 Powered by <a className='hover:underline' href="https://lizhi.us.kg" target="_blank">Qing</a><br />
+                        知不可乎骤得,托遗响于悲风。
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
